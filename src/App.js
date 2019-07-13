e869c2a89bbfe38f76a550b3416bd277315e6e4c
Mohammad Abdullah <mohd.a.saed@gmail.com>
2019-07-13 13:13:53 +0300

    Install react-router-dom and create Home container

--- src/App.js
+++ src/App.js
@@ -4,11 +4,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'jquery/dist/jquery.min.js';
 import 'popper.js/dist/umd/popper.min.js';
 import 'bootstrap/dist/js/bootstrap.min.js';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import './App.css';
+import Home from './containers/Home/Home';
 
 function App() {
-  return <div className="container">Hello, World!</div>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/containers/Home/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="h1">Home Container</div>
+      </div>
+    );
+  }
+}
+
+export default Home;
